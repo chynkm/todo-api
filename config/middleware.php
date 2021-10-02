f16536fc9ef310c3de24751ece8f7f1a304d0c54
Karthik M <chynkm@gmail.com>
2021-10-02 19:25:56 +0530

    Basic SLIM setup

---
+++ config/middleware.php
@@ -0,0 +1,11 @@
+<?php
+
+use Slim\App;
+
+return function (App $app) {
+    // Parse json, form data and xml
+    $app->addBodyParsingMiddleware();
+
+    // Add the Slim built-in routing middleware
+    $app->addRoutingMiddleware();
+};
---
+++ config/settings.php
@@ -0,0 +1,18 @@
+<?php
+
+// change to 0 in production
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+$settings['root'] = dirname(__DIR__);
+
+// Error Handling Middleware settings
+$settings['error'] = [
+
+    // change to false in production
+    'display_error_details' => true,
+    'log_errors' => true,
+    'log_error_details' => true,
+];
+
+return $settings;
